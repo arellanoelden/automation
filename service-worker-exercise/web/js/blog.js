b1c176ba53509c346c911f74f46ad2ab810a5ea6
Elden Arellano <eldena@CRS-EAREL-MAC.corp.ashn.com>
2020-01-13 15:45:23 -0800

    finish sw and get offline working for ramblings

--- service-worker-exercise/web/js/blog.js
+++ service-worker-exercise/web/js/blog.js
@@ -1,75 +1,91 @@
-(function Blog(){
+(function Blog(global){
 	"use strict";
 
 	var offlineIcon;
-	var isOnline = ("onLine" in navigator) ? navigator.onLine : true;
+	var isOnline = ("onLine" in navigator) && navigator.onLine;
 	var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
 	var usingSW = ("serviceWorker" in navigator);
 	var swRegistration;
 	var svcworker;
 
+	if (usingSW) {
+		initServiceWorker().catch(console.error);
+	}
+
+	global.isBlogOnline = isBlogOnline;
+
 	document.addEventListener("DOMContentLoaded",ready,false);
 
-	initServiceWorker().catch(console.error);
+
 	// **********************************
 
 	function ready() {
 		offlineIcon = document.getElementById("connectivity-status");
-		
-		if(!isOnline) {
+
+		if (!isOnline) {
 			offlineIcon.classList.remove("hidden");
 		}
 
-		window.addEventListener("online", function online() {
+		window.addEventListener("online",function online(){
 			offlineIcon.classList.add("hidden");
 			isOnline = true;
 			sendStatusUpdate();
-		});
-
-		window.addEventListener("offline", function offline() {
+		},false);
+		window.addEventListener("offline",function offline(){
 			offlineIcon.classList.remove("hidden");
 			isOnline = false;
 			sendStatusUpdate();
-		})
+		},false);
+	}
+
+	function isBlogOnline() {
+		return isOnline;
 	}
 
 	async function initServiceWorker() {
-		swRegistration = await navigator.serviceWorker.register("/sw.js", {
-			updateViaCache: "none"
+		swRegistration = await navigator.serviceWorker.register("/sw.js",{
+			updateViaCache: "none",
 		});
 
 		svcworker = swRegistration.installing || swRegistration.waiting || swRegistration.active;
+		sendStatusUpdate(svcworker);
 
-		navigator.serviceWorker.addEventListener("controllerchange", function onController() {
+		// listen for new service worker to take over
+		navigator.serviceWorker.addEventListener("controllerchange",async function onController(){
 			svcworker = navigator.serviceWorker.controller;
 			sendStatusUpdate(svcworker);
 		});
 
-		navigator.serviceWorker.addEventListener("message", onSWMessage);
+		navigator.serviceWorker.addEventListener("message",onSWMessage,false);
 	}
 
 	function onSWMessage(evt) {
 		var { data } = evt;
-		if(data.requestStatusUpdate) {
-			console.log("Received status update request from service worker, responding...")
+		if (data.statusUpdateRequest) {
+			console.log("Status update requested from service worker, responding...");
 			sendStatusUpdate(evt.ports && evt.ports[0]);
+		}
+		else if (data == "force-logout") {
+			document.cookie = "isLoggedIn=";
+			isLoggedIn = false;
+			sendStatusUpdate();
 		}
 	}
 
 	function sendStatusUpdate(target) {
-		sendSWMessage({ statusUpdate: { isOnline, isLoggedIn }}, target);
+		sendSWMessage({ statusUpdate: { isOnline, isLoggedIn } },target);
 	}
 
-	async function sendSWMessage(msg, target) {
-		if(target) {
+	function sendSWMessage(msg,target) {
+		if (target) {
 			target.postMessage(msg);
 		}
-		else if(svcworker) {
+		else if (svcworker) {
 			svcworker.postMessage(msg);
 		}
-		else {
+		else if (navigator.serviceWorker.controller) {
 			navigator.serviceWorker.controller.postMessage(msg);
 		}
 	}
 
-})();
+})(window)
